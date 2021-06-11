705cda67d2059a349d6e0df77da7a5dac52b1bb9
Shireen <shirin@haybatov.com>
2021-06-11 18:17:27 +1000

    added exercise 5

---
+++ Sev_asgm_5.py
@@ -0,0 +1,21 @@
+count = 0
+total = 0
+while True:
+    snum = input("Enter a number: ")
+    if snum == "done":
+        break        
+    try:
+        fnum = float(snum)
+    except:
+        print("Invalid input")
+        continue
+    
+    count = count + 1
+    total = total + fnum
+ 
+if count == 0:
+    print("NO input")
+else:
+    print(total, count, total / count)
+
+    
